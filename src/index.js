a03802803063905bae8b7729c7ecdb166dc30baa
Alexandr <yaromahob@gmail.com>
2021-08-08 18:54:32 +0300

    feat: task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let finish = "" + n;
+    finish = finish.split("").reverse().slice(0, 3).join("");
+    return Number(finish);
+};
